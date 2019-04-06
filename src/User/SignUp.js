5e499f45491e65fb0047c831ba2135285e3fb857
PalaciosZ <criszelaya24@hotmail.com>
2019-04-05 22:13:37 -0600

    Creating signup form

--- src/User/SignUp.js
+++ src/User/SignUp.js
@@ -1,11 +1,56 @@
 import React from 'react'
 import  {withRouter} from 'react-router-dom';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
 
 function Signup() {
   return(
-    <div>
-    <h1>testing..</h1>
-    </div>
+    <MDBContainer className="center">
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard
+            className="card-image"
+            style={{
+              backgroundImage:
+                "url(https://mdbootstrap.com/img/Photos/Others/pricing-table7.jpg)",
+              width: "23rem"
+            }}
+          >
+            <div className="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
+              <div className="text-center">
+                <h3 className="white-text mb-5 mt-4 font-weight-bold">
+                  <strong>SIGN</strong>
+                  <a href="#!" className="green-text font-weight-bold">
+                    <strong> UP</strong>
+                  </a>
+                </h3>
+              </div>
+              <MDBInput label="Your email" group type="text" validate />
+              <MDBInput label="Your password" group type="password" validate />
+              <MDBRow className="d-flex align-items-center mb-4">
+                <div className="text-center mb-3 col-md-12">
+                  <MDBBtn
+                    color="success"
+                    rounded
+                    type="button"
+                    className="btn-block z-depth-1"
+                  >
+                    Sign in
+                  </MDBBtn>
+                </div>
+              </MDBRow>
+              <MDBCol md="12">
+                <p className="font-small white-text d-flex justify-content-end">
+                  Have an account?
+                  <a href="#!" className="green-text ml-1 font-weight-bold">
+                    Log in
+                  </a>
+                </p>
+              </MDBCol>
+            </div>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   )
 }
 
