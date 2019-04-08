5f1f86469c0b4220e54dbbaa6d83fcf7cd7503a1
PalaciosZ <criszelaya24@hotmail.com>
2019-04-08 15:46:14 -0600

    Styling css

--- src/User/SignUp.js
+++ src/User/SignUp.js
@@ -1,57 +1,54 @@
 import React from 'react'
 import  {withRouter} from 'react-router-dom';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
 
-function Signup() {
-  return(
-    <MDBContainer className="center">
-      <MDBRow>
-        <MDBCol md="6">
-          <MDBCard
-            className="card-image"
-            style={{
-              backgroundImage:
-                "url(https://mdbootstrap.com/img/Photos/Others/pricing-table7.jpg)",
-              width: "23rem"
-            }}
-          >
-            <div className="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
-              <div className="text-center">
-                <h3 className="white-text mb-5 mt-4 font-weight-bold">
-                  <strong>SIGN</strong>
-                  <a href="#!" className="green-text font-weight-bold">
-                    <strong> UP</strong>
-                  </a>
-                </h3>
-              </div>
-              <MDBInput label="Your email" group type="text" validate />
-              <MDBInput label="Your password" group type="password" validate />
-              <MDBRow className="d-flex align-items-center mb-4">
-                <div className="text-center mb-3 col-md-12">
-                  <MDBBtn
-                    color="success"
-                    rounded
-                    type="button"
-                    className="btn-block z-depth-1"
-                  >
-                    Sign in
-                  </MDBBtn>
-                </div>
-              </MDBRow>
-              <MDBCol md="12">
-                <p className="font-small white-text d-flex justify-content-end">
-                  Have an account?
-                  <a href="#!" className="green-text ml-1 font-weight-bold">
-                    Log in
-                  </a>
-                </p>
-              </MDBCol>
-            </div>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  )
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    }
+  }
+
+  render(){
+    return(
+      <div class="container test">
+      <img class="rounded-circle img" src={require('../img/signup.jpg')} />
+      <h1> Welcome to Chitter App!</h1>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label>Full name</label>
+          <input type="text" class="form-control" placeholder="Enter your name"/>
+          <small class="form-text text-muted">What's your name?.</small>
+      </div>
+      <div class="form-group">
+        <label>username</label>
+        <input type="text" class="form-control" placeholder="Username"/>
+    </div>
+      </div>
+      <div class="col">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+      </div>
+    </div>
+    </div>
+    <div class= "input-group mb-3 small-select">
+    <select class="custom-select" id="inputGroupSelect01">
+        <option selected>Choose...</option>
+        <option value="F">Female</option>
+        <option value="M">Male</option>
+    </select>
+    </div>
+    <button type="submit" class="btn btn-primary large-btn">Submit</button>
+  </div>
+    )
+  }
 }
 
 export default withRouter(Signup)
